bd105bbe9ad062e8cefa36bf881637c9b5b64a5e
q-tran14 <tranquangquan.it@gmail.com>
2025-05-28 12:32:47 +0700

    Fix

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,34 +1,34 @@
 import React from "react";
 
 export default function Contact() {
-  const [result, setResult] = React.useState("");
+//   const [result, setResult] = React.useState("");
 
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    setResult("Sending....");
-    const formData = new FormData(event.target);
+//   const onSubmit = async (event) => {
+//     event.preventDefault();
+//     setResult("Sending....");
+//     const formData = new FormData(event.target);
 
-    formData.append("access_key", "cb65ff76-63d1-43ed-9d19-68dcae2287fa");
+//     formData.append("access_key", "cb65ff76-63d1-43ed-9d19-68dcae2287fa");
 
-    const response = await fetch("https://api.web3forms.com/submit", {
-      method: "POST",
-      body: formData
-    });
+//     const response = await fetch("https://api.web3forms.com/submit", {
+//       method: "POST",
+//       body: formData
+//     });
 
-    const data = await response.json();
+//     const data = await response.json();
 
-    if (data.success) {
-      setResult("Form Submitted Successfully");
-      event.target.reset();
-    } else {
-      console.log("Error", data);
-      setResult(data.message);
-    }
-  };
+//     if (data.success) {
+//       setResult("Form Submitted Successfully");
+//       event.target.reset();
+//     } else {
+//       console.log("Error", data);
+//       setResult(data.message);
+//     }
+//   };
 
   return (
     <div className="min-h-screen p-6 bg-gray-900 text-white flex flex-col max-w-4xl mx-auto">
-      <h1 className="text-4xl font-bold mb-8 text-center">Contact Me</h1>
+      {/* <h1 className="text-4xl font-bold mb-8 text-center">Contact Me</h1>
       <form onSubmit={onSubmit} className="flex flex-col gap-4">
         <input
           type="text"
@@ -65,7 +65,7 @@ export default function Contact() {
           Shoot a Message
         </button>
       </form>
-      <span className="mt-4 text-center">{result}</span>
+      <span className="mt-4 text-center">{result}</span> */}
     </div>
   );
 }
